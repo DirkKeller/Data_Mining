c8940b63959c77fae98c77c8871678c04304f5a2
meshedsilver <d.keller@students.uu.nl>
2022-10-23 12:17:49 +0200

    hyperparam tuning

--- project2.py
+++ project2.py
@@ -1,5 +1,3 @@
-import sklearn
-from sklearn.naive_bayes import MultinomialNB
 import os
 import pandas as pd
 import csv
@@ -7,79 +5,104 @@ import string
 import re
 import nltk
 
-
-# from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-# from nltk.corpus import stopwords
-# from nltk.corpus import wordnet
-
-# def read_text(wd):
-
-#     for i in range(1, 5): #6
-#         # Change the directory
-#         path= wd + "\\fold" + str(i)
-#         os.chdir(path)
-#         # iterate through all file
-#         for idx, file in enumerate(os.listdir()):
-#             # Check whether file is in text format or not
-#             file_path = f"{path}\\{file}"
-           
-#             if idx==0 and i==1 :
-#                 X=pd.read_csv(file_path, header=None,sep='\s\s+', quoting=csv.QUOTE_NONE, encoding='utf-8', index_col=False, engine="python")
-#             else:
-#                 X=pd.concat([X,pd.read_csv(file_path, header=None,sep='\s\s+', quoting=csv.QUOTE_NONE, encoding='utf-8', index_col=False, engine="python")])
-#     X.columns=['review']
-#     return X
-
-# def preprocess_sentence(data: pd.DataFrame) -> pd.DataFrame:
-#     # Remove punctuation
-#     data['review'] = data['review'].apply(lambda text: "".join([i for i in text if i not in string.punctuation]))
-#     # Remove capital letters
-#     data['review'] = data['review'].apply(lambda text: text.lower())
-#     # Remove numbers
-#     data['review'] = data['review'].apply(lambda text: ''.join([i for i in text if not i.isdigit()]))
-    
-#     # Tokenize words
-#     data['review'] = data['review'].apply(lambda text: re.split(r'\s+',text))
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
 
 
-#     # Lemmatize words
-#     word_lemmatizer=nltk.stem.WordNetLemmatizer()
-#     data['review'] = data['review'].apply(lambda text: ' '.join([word_lemmatizer.lemmatize(word) for word in text]))  # , [get_wordnet_pos(word)]
-#     return data
+def read_text(wd, folds, class_label):
+    start = True
+    for i in folds:  # 6
+        # Change the directory
+        path = wd + "\\fold" + str(i)
+        os.chdir(path)
 
-# # Download NLP corpora
-# nltk.download('wordnet')
-# nltk.download('omw-1.4')
-# nltk.download('averaged_perceptron_tagger')
+        # iterate through all file
+        for idx, file in enumerate(os.listdir()):
+            # Check whether file is in text format or not
+            file_path = f"{path}\\{file}"
 
-# # Folder Path
-# path = os.path.join(os.getcwd(), "negative_polarity")
-# folder_list = ["\\deceptive_from_MTurk", "\\truthful_from_Web"]
+            if start:
+                X = pd.read_csv(file_path, header=None, sep='\s\s+', quoting=csv.QUOTE_NONE, encoding='utf-8',
+                                index_col=False, engine="python")
+                start = False
+            else:
+                X = pd.concat([X, pd.read_csv(file_path, header=None, sep='\s\s+', quoting=csv.QUOTE_NONE,
+                                              encoding='utf-8', index_col=False, engine="python")])
+    X.columns = ['review']
 
-# # Collect deceptive (0) and truthful (1) reviews
-# data = pd.concat([read_text(path+folder_list[0]), read_text(path+folder_list[1])])
-# prep = preprocess_sentence(data)
+    X['class'] = [class_label] * X.shape[0]
+    return X
 
-# # Vectorize the data excluding stopwords
-# stop = stopwords.words('english')
 
-# ngram_range = (1,2)
-# max_features = 1000
-# use_idf = True
+def preprocess_sentence(data: pd.DataFrame) -> pd.DataFrame:
+    # Remove punctuation
+    data['review'] = data['review'].apply(lambda text: "".join([i for i in text if i not in string.punctuation]))
+    # Remove capital letters
+    data['review'] = data['review'].apply(lambda text: text.lower())
+    # Remove numbers
+    data['review'] = data['review'].apply(lambda text: ''.join([i for i in text if not i.isdigit()]))
+    # Tokenize words
+    data['review'] = data['review'].apply(lambda text: re.split(r'\s+', text))
+    # Lemmatize words
+    word_lemmatizer = nltk.stem.WordNetLemmatizer()
+    data['review'] = data['review'].apply(
+        lambda text: ' '.join([word_lemmatizer.lemmatize(word) for word in text]))  # , [get_wordnet_pos(word)]
+    return data
 
-# vector = TfidfVectorizer(max_features=max_features,
-#                         ngram_range=ngram_range,
-#                         min_df=1, 
-#                         max_df=1.0,
-#                         use_idf=use_idf)
 
-# vec=vector.fit_transform(prep['review'])
-# df = pd.DataFrame(vec.toarray().transpose(),index=vector.get_feature_names())
-# df.columns=['Rev ' + str(i) for i, _ in enumerate(df.columns)]
-# df = df.T
-# df['class'] = [0]*320 + [1]*320
-# os.chdir(path)
-# df.to_csv('reviews_document_term.csv', index=False)
+# Download NLP corpora
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('averaged_perceptron_tagger')
+
+# Folder Path
+wd = os.getcwd()
+path = os.path.join(wd, "negative_polarity")
+folder_list = ["\\deceptive_from_MTurk", "\\truthful_from_Web"]
+
+# Train: Collect deceptive (0) and truthful (1) reviews
+train_data = pd.concat([read_text(wd=path + folder_list[0], folds=range(1, 5), class_label=0),
+                        read_text(wd=path + folder_list[1], folds=range(1, 5), class_label=1)])
+train_prep = preprocess_sentence(train_data)
+
+# Test set: Collect deceptive (0) and truthful (1) reviews
+test_data = pd.concat([read_text(wd=path + folder_list[0], folds=range(5, 6), class_label=0),
+                       read_text(wd=path + folder_list[1], folds=range(5, 6), class_label=1)])
+test_prep = preprocess_sentence(test_data)
+
+# Vectorize the data excluding stopwords
+stop = stopwords.words('english')
+ngram_range = (1, 2)
+max_features = 1000
+use_idf = True
+
+vector = TfidfVectorizer(max_features=max_features,
+                         ngram_range=ngram_range,
+                         min_df=1,
+                         max_df=1.0,
+                         use_idf=use_idf)
+
+train_vec = vector.fit_transform(train_prep['review'])
+test_vec = vector.transform(test_prep['review'])
+
+# Train set: Construct document-term matrix
+os.chdir(path)
+train_df = pd.DataFrame(train_vec.toarray().transpose(), index=vector.get_feature_names())
+train_df.columns = ['Rev ' + str(i) for i, _ in enumerate(train_df.columns)]
+train_df = pd.concat([train_df.T, train_prep['class']], axis=1)
+train_df.to_csv('train_reviews_document_term.csv')
+
+# Test set: Construct document-term matrix 
+test_df = pd.DataFrame(test_vec.toarray().transpose(), index=vector.get_feature_names())
+test_df.columns = ['Rev ' + str(i) for i, _ in enumerate(test_df.columns)]
+test_df = pd.concat([test_df.T, test_prep['class']], axis=1)
+test_df.to_csv('test_reviews_document_term.csv')
+
 # term_document_matrix['total_count'] = term_document_matrix.sum(axis=1)
 
 # # Top 25 words
@@ -93,69 +116,66 @@ import nltk
 # from sklearn.model_selection import KFold
 # kf = KFold(n_splits=5, shuffle = False).split(range(25))
 
-# k_range = list(range(1, 31))
-# k_scores = []
-# for k in k_range:    
-#    knn = KNeighborsClassifier(n_neighbors=k)    
-#    scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy')
-#    k_scores.app
-
-# import matplotlib.pyplot as plt
-# %matplotlib inline
-
-# # plot the value of K for KNN (x-axis) versus the cross-validated accuracy (y-axis)
-# plt.plot(k_range, k_scores)
-# plt.xlabel('Value of K for KNN')
-# plt.ylabel('Cross-Validated Accuracy')
-
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
-import pandas as pd
-
-
-#import data
-path = os.path.join(os.getcwd(), "negative_polarity")
+# Import data
+path = os.path.join(wd, "negative_polarity")
 os.chdir(path)
-data= pd.read_csv('reviews_document_term.csv')
-vec=data.drop('class', axis=1)
-""" define the parameter values that should be searched """
-# logistic regression
-C_range = list(range(1, 1000))
-nmin_range
-min_leafs
-ccp_alpha
+train_df = pd.read_csv('train_reviews_document_term.csv', header=0, index_col=0)
+test_df = pd.read_csv('test_reviews_document_term.csv', header=0, index_col=0)
 
+train_y = train_df['class']
+test_y = test_df['class']
+train_x = train_df.drop('class', axis=1)
+test_x = test_df.drop('class', axis=1)
 
-# specify "parameter distributions" rather than a "parameter grid"
-#param_dist = dict(n_neighbors=k_range, weights=weight_options)
-param_dist = dict(C=C_range) #, weights=weight_options)
-param_dist_dt_r = dict(min_samples_split=nmin_range, min_samples_leaf=min_leafs)
+""" Define the parameter values and distributions"""
+# Naive Bayes
+smooth = list(range(start=0, stop=1, step=0.01))
 
+# Logistic regression
+C = list(range(start=1, stop=100, step=1))
+reg = ['l1', 'l2', 'elasticnet']
 
-    # min_samples_split=2, 
-    # min_samples_leaf=1, 
-    # min_weight_fraction_leaf=0.0, 
-    # max_features=None, 
+# Decision tree
+ccp = list(range(start=0, stop=1, step=0.01))
+imp = ['gini', 'entropy', 'log_loss']
+
+# Random Forest
+m = list(range(start=100, stop=1000, step=1))
+nfeat = list
+
+param_dist_nb = dict(alpha=smooth)
+param_dist_lr = dict(C=C)
+param_dist_dt = dict(ccp_alpha=ccp, criterion=imp)
+param_dist_rf = dict(ccp_alpha=ccp, max_features=nfeat, n_estimators=m, criterion=imp)
+
 # Initialize models
-nb=MultinomialNB()
-lr=LogisticRegression(penalty='l1', solver='saga', random_state=5)
-dt = DecisionTreeClassifier(
-    criterion='gini', 
-    splitter='best',  
-    random_state=5)
+nb = MultinomialNB()
+lr = LogisticRegression(solver='saga',
+                        random_state=5,
+                        n_jobs=os.cpu_count(),
+                        max_iterint=250)
+dt = DecisionTreeClassifier(random_state=5,
+                            n_jobs=os.cpu_count())
+rf = RandomForestClassifier(criterion='gini',
+                            random_state=5,
+                            n_jobs=os.cpu_count())
+models = [nb, lr, dt, rf]
+param_dists = [param_dist_nb, param_dist_lr, param_dist_dt, param_dist_rf]
 
-rand = RandomizedSearchCV(lr,
-                          param_dist,
-                          cv=10,
-                          scoring='accuracy',
-                          n_iter=50,
-                          random_state=5,
-                          return_train_score=False)
-rand.fit(vec, data['class'])
-pd.DataFrame(rand.cv_results_)[['mean_test_score', 'std_test_score', 'params']]
+# Train
+selection = []
+for i, _ in enumerate(models):
+    rand = RandomizedSearchCV(models[i],
+                              param_dists[i],
+                              cv=2,  # 20
+                              scoring=['accuracy', 'f1'],
+                              n_iter=1,  # 100
+                              random_state=5,
+                              return_train_score=False,
+                              verbose=1)
+    rand.fit(train_x, train_y)
+    pd.DataFrame(rand.cv_results_)[['mean_test_score', 'std_test_score', 'params']]
 
-# examine the best model
-print(rand.best_score_)
-print(rand.best_params_)
+    selection.append(f'{models[i]}: Best parameters {rand.best_score_}, best score  {rand.best_score_}')
+
+selection
