d4a94a8f9ca2201b7f32f582d7efcc67e28e1123
DirkKeller <70960084+DirkKeller@users.noreply.github.com>
2022-12-14 01:25:19 +0100

    clean up

--- Project_1/project1_decisiontree_noclass.py
+++ Project_1/project1_decisiontree_noclass.py
@@ -7,8 +7,6 @@ import pandas as pd
 import numpy as np
 from anytree import Node, RenderTree
 
-
-
 def tree_grow(x: np.ndarray, y: np.ndarray, nmin: int, minleaf: int, nfeat: int) -> RenderTree:
     """
     A tree expansion function that builds a single decision tree depending on the nmin, minleaf, nfeat
@@ -228,58 +226,3 @@ def split(parent: Node, feat_idx: int, pos, minleaf) -> tuple:  # add the contra
         Rchild = Node(f'R {tree_iter}', features=parent.features[parent.features[:, feat_idx] < pos],
                         label=parent.label[parent.features[:, feat_idx] < pos])
     return Lchild, Rchild
-
-
-
-from numpy import genfromtxt
-import numpy as np
-import pandas as pd
-import time
-
-# Basic test on credit data. Prediction should be perfect.
-
-credit_data = genfromtxt('C:\\Users\\giada\\Documents\\GitHub\\Data_Mining\\credit.txt', delimiter=',', skip_header=True)
-credit_x = credit_data[:,0:5]
-credit_y = credit_data[:,5]
-credit_tree = tree_grow(credit_x,credit_y,2,1,5)
-credit_pred = tree_pred(credit_x, credit_tree)
-pd.crosstab(credit_y,credit_pred)
-
-# Single tree on pima data
-
-pima_data = genfromtxt('C:\\Users\\giada\\Documents\\GitHub\\Data_Mining\\pima.txt', delimiter=',')
-pima_x = pima_data[:,0:8]
-pima_y = pima_data[:,8]
-pima_tree = tree_grow(pima_x,pima_y,20,5,8)
-pima_pred = tree_pred(pima_x,pima_tree)
-pd.crosstab(pima_y,pima_pred)
-
-# Function for testing single tree
-
-def single_test(x,y,nmin,minleaf,nfeat,n):
-  acc = np.zeros(n)
-  for i in range(0, n):
-    tr = tree_grow(x,y,nmin,minleaf,nfeat)
-    pred = tree_pred(x,tr)
-    acc[i] = sum(pred == y)/len(y)
-  return [np.mean(acc), np.std(acc)]
-
-# Function for testing bagging/random forest
-
-def rf_test(x,y,nmin,minleaf,nfeat,m,n):
-  acc = np.zeros(n)
-  for i in range(0,n):
-    tr_list = tree_grow_b(x,y,nmin,minleaf,nfeat,m)
-    pred = tree_pred_b(x,tr_list)
-    acc[i] = sum(pred == y)/len(y)
-  return [np.mean(acc), np.std(acc)]
-
-# Compute average and standard deviation of accuracy for single tree
-
-single_test(pima_x,pima_y,20,5,2,25)
-single_test(pima_x,pima_y,20,5,8,25)
-
-# Compute average and standard deviation of accuracy for bagging/random forest
-
-rf_test(pima_x,pima_y,20,5,2,25,25)
-rf_test(pima_x,pima_y,20,5,8,25,25)
--- Project_1/test_cases.py
+++ Project_1/test_cases.py
@@ -1,18 +1,12 @@
+from project1_decisiontree_noclass import tree_grow,tree_pred,tree_grow_b,tree_pred_b
 from numpy import genfromtxt
-
 import numpy as np
-
 import pandas as pd
-
 import time
-
-
 
 # Basic test on credit data. Prediction should be perfect.
 
-
-
-credit_data = genfromtxt('C:/Data Mining/credit.txt', delimiter=',', skip_header=True)
+credit_data = genfromtxt('C:\\Users\\giada\\Documents\\GitHub\\Data_Mining\\credit.txt', delimiter=',', skip_header=True)
 credit_x = credit_data[:,0:5]
 credit_y = credit_data[:,5]
 credit_tree = tree_grow(credit_x,credit_y,2,1,5)
@@ -20,7 +14,8 @@ credit_pred = tree_pred(credit_x, credit_tree)
 pd.crosstab(credit_y,credit_pred)
 
 # Single tree on pima data
-pima_data = genfromtxt('C:/Data Mining/pima.txt', delimiter=',')
+
+pima_data = genfromtxt('C:\\Users\\giada\\Documents\\GitHub\\Data_Mining\\pima.txt', delimiter=',')
 pima_x = pima_data[:,0:8]
 pima_y = pima_data[:,8]
 pima_tree = tree_grow(pima_x,pima_y,20,5,8)
@@ -28,6 +23,7 @@ pima_pred = tree_pred(pima_x,pima_tree)
 pd.crosstab(pima_y,pima_pred)
 
 # Function for testing single tree
+
 def single_test(x,y,nmin,minleaf,nfeat,n):
   acc = np.zeros(n)
   for i in range(0, n):
@@ -37,9 +33,9 @@ def single_test(x,y,nmin,minleaf,nfeat,n):
   return [np.mean(acc), np.std(acc)]
 
 # Function for testing bagging/random forest
+
 def rf_test(x,y,nmin,minleaf,nfeat,m,n):
   acc = np.zeros(n)
-
   for i in range(0,n):
     tr_list = tree_grow_b(x,y,nmin,minleaf,nfeat,m)
     pred = tree_pred_b(x,tr_list)
@@ -52,6 +48,6 @@ single_test(pima_x,pima_y,20,5,2,25)
 single_test(pima_x,pima_y,20,5,8,25)
 
 # Compute average and standard deviation of accuracy for bagging/random forest
+
 rf_test(pima_x,pima_y,20,5,2,25,25)
 rf_test(pima_x,pima_y,20,5,8,25,25)
-
